732d674d2cf8675e3adfe363944ee0807da20db7
Nathan <ntan97@gmail.com>
2016-11-10 12:30:05 -0700

    Initial support for adding custom field support

--- includes/authenticate.php
+++ includes/authenticate.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Stormpath WordPress is a WordPress plugin to authenticate against a Stormpath Directory.
- * Copyright (C) 2016  Stormpath
+ * Copyright (C) 2016  Stormpath.
  *
  * This file is part of Stormpath WordPress.
  *
@@ -17,8 +17,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package Stormpath\WordPress;
  */
 
 namespace Stormpath\WordPress;
@@ -29,268 +27,291 @@ use WP_Error;
 use WP_User;
 
 /**
- * Class Authenticate
+ * Class Authenticate.
  *
  * @category    Stormpath
- * @package     Stormpath\WordPress
+ *
  * @author      Stormpath <support@stormpath.com>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ *
  * @link        https://stormpath.com/
  */
-class Authenticate
+class authenticate
 {
-	/**
-	 * The Stormpath Client.
-	 *
-	 * @var Client
-	 */
-	private $spClient;
-	/**
-	 * The Stormpath Application.
-	 *
-	 * @var Application
-	 */
-	private $spApplication;
+    /**
+     * The Stormpath Client.
+     *
+     * @var Client
+     */
+    private $spClient;
+    /**
+     * The Stormpath Application.
+     *
+     * @var Application
+     */
+    private $spApplication;
 
-	/**
-	 * Authenticate constructor.
-	 *
-	 * @param Application       $application The Stormpath Application.
-	 * @param \Stormpath\Client $client      The Stormpath Client.
-	 */
-	public function __construct( $application, \Stormpath\Client $client ) {
-		$this->spClient = $client;
-		$this->spApplication = $application;
+    /**
+     * Authenticate constructor.
+     *
+     * @param Application       $application The Stormpath Application.
+     * @param \Stormpath\Client $client      The Stormpath Client.
+     */
+    public function __construct($application, \Stormpath\Client $client)
+    {
+        $this->spClient = $client;
+        $this->spApplication = $application;
+    }
 
-	}
+    /**
+     * The main authenticate method.
+     *
+     * @param null|WP_User|WP_Error $user     The WordPress User.
+     * @param string                $username Username.
+     * @param string                $password Password.
+     *
+     * @return bool|WP_Error|WP_User
+     */
+    public function authenticate($user, $username, $password)
+    {
+        remove_action('authenticate', 'wp_authenticate_username_password', 20);
 
-	/**
-	 * The main authenticate method.
-	 *
-	 * @param null|WP_User|WP_Error $user     The WordPress User.
-	 * @param string                $username Username.
-	 * @param string                $password Password.
-	 * @return bool|WP_Error|WP_User
-	 */
-	public function authenticate( $user, $username, $password ) {
-		remove_action( 'authenticate', 'wp_authenticate_username_password', 20 );
+        if (empty($username) || empty($password)) {
+            if (is_wp_error($user)) {
+                return $user;
+            }
 
-		if ( empty( $username ) || empty( $password ) ) {
-			if ( is_wp_error( $user ) ) {
-				return $user;
-			}
+            $error = new WP_Error();
 
-			$error = new WP_Error();
+            if (empty($username)) {
+                $error->add('empty_username', __('<strong>ERROR</strong>: The username field is empty.'));
+            }
 
-			if ( empty( $username ) ) {
-				$error->add( 'empty_username', __( '<strong>ERROR</strong>: The username field is empty.' ) );
-			}
+            if (empty($password)) {
+                $error->add('empty_password', __('<strong>ERROR</strong>: The password field is empty.'));
+            }
 
-			if ( empty( $password ) ) {
-				$error->add( 'empty_password', __( '<strong>ERROR</strong>: The password field is empty.' ) );
-			}
+            return $error;
+        }
 
-			return $error;
-		}
+        $authenticationRequest = new \Stormpath\Authc\UsernamePasswordRequest(
+            $username,
+            $password
+        );
 
-		$authenticationRequest = new \Stormpath\Authc\UsernamePasswordRequest(
-			$username,
-			$password
-		);
+        $account = null;
 
-		$account = null;
+        try {
+            $account = $this->spApplication->authenticateAccount($authenticationRequest)->account;
+        } catch (\Exception $e) {
+            if (7104 === $e->getCode()) {
+                $account = $this->attempt_normal_login($username, $password);
+            } else {
+                return false;
+            }
+        }
 
-		try {
-			$account = $this->spApplication->authenticateAccount( $authenticationRequest )->account;
-		} catch (\Exception $e) {
-		    if ( 7104 === $e->getCode() ) {
-				$account = $this->attempt_normal_login( $username, $password );
-			} else {
-				return false;
-			}
-		}
+        if ($account instanceof WP_Error) {
+            return $account;
+        }
 
-		if ( $account instanceof WP_Error ) {
-		    return $account;
-		}
+        if (!$account instanceof \Stormpath\Resource\Account) {
+            return false;
+        }
 
-		if ( ! $account instanceof \Stormpath\Resource\Account ) {
-		    return false;
-		}
+        $wpUser = $this->get_wp_user($account);
 
-		$wpUser = $this->get_wp_user( $account );
+        if (0 === $wpUser->ID) {
+            $wpUser = $this->create_wp_user($account, $password);
+        }
 
-		if ( 0 === $wpUser->ID ) {
-			$wpUser = $this->create_wp_user( $account, $password );
-		}
+        return $this->login_wp_user($wpUser);
+    }
 
-		return $this->login_wp_user( $wpUser );
+    /**
+     * Attempt a normal login with WordPress core function.
+     *
+     * @param string $username The username of login attempt.
+     * @param string $password The password of the login attempt.
+     *
+     * @return bool|\Stormpath\Resource\Account
+     */
+    private function attempt_normal_login($username, $password)
+    {
+        $user = get_user_by('login', $username);
 
-	}
+        if ($user && wp_check_password($password, $user->data->user_pass, $user->ID)) {
+            return $this->register_stormpath_user($user, $password);
+        }
 
-	/**
-	 * Attempt a normal login with WordPress core function.
-	 *
-	 * @param string $username The username of login attempt.
-	 * @param string $password The password of the login attempt.
-	 * @return boolean|\Stormpath\Resource\Account
-	 */
-	private function attempt_normal_login( $username, $password ) {
-		$user = get_user_by( 'login', $username );
+        $user = get_user_by('email', $username);
 
-		if ( $user && wp_check_password( $password, $user->data->user_pass, $user->ID ) ) {
-			return $this->register_stormpath_user( $user, $password );
-		}
+        if ($user && wp_check_password($password, $user->data->user_pass, $user->ID)) {
+            return $this->register_stormpath_user($user, $password);
+        }
 
-		$user = get_user_by( 'email', $username );
+        return false;
+    }
 
-		if ( $user && wp_check_password( $password, $user->data->user_pass, $user->ID ) ) {
-			return $this->register_stormpath_user( $user, $password );
-		}
+    /**
+     * Get a WordPress user based on the account email.
+     *
+     * @param Account $account The stormpath account.
+     *
+     * @return WP_User
+     */
+    private function get_wp_user(Account $account)
+    {
+        return new WP_User((new WP_User())->get_data_by('email', $account->email)->ID);
+    }
 
-		return false;
-	}
+    /**
+     * Create a WordPress User.
+     *
+     * @param Account $account  The Stormpath Account.
+     * @param string  $password The Password.
+     *
+     * @return WP_User
+     */
+    private function create_wp_user(Account $account, $password)
+    {
+        $userAccountInfo = [
+            'user_email' => $account->email,
+            'user_login' => $account->username,
+            'first_name' => $account->givenName,
+            'last_name' => $account->surname,
+            'user_pass' => wp_hash_password(wp_generate_password(32)),
+        ];
 
-	/**
-	 * Get a WordPress user based on the account email.
-	 *
-	 * @param Account $account The stormpath account.
-	 * @return WP_User
-	 */
-	private function get_wp_user( Account $account ) {
-		return new WP_User( (new WP_User())->get_data_by( 'email', $account->email )->ID );
+        $newUser = wp_insert_user($userAccountInfo);
 
-	}
+        return new WP_User($newUser);
+    }
 
-	/**
-	 * Create a WordPress User.
-	 *
-	 * @param Account $account  The Stormpath Account.
-	 * @param string  $password The Password.
-	 * @return WP_User
-	 */
-	private function create_wp_user( Account $account, $password ) {
-		$userAccountInfo = [
-			'user_email'    => $account->email,
-			'user_login'    => $account->username,
-			'first_name'    => $account->givenName,
-			'last_name'     => $account->surname,
-			'user_pass'     => wp_hash_password( wp_generate_password( 32 ) ),
-		];
+    /**
+     * Force Login of WordPress User.
+     *
+     * @param WP_User $wpUser The WordPress User.
+     *
+     * @return WP_User
+     */
+    public function login_wp_user($wpUser)
+    {
+        return wp_set_current_user($wpUser->ID);
+    }
 
-		$newUser = wp_insert_user( $userAccountInfo );
+    /**
+     * Hook callback for when a user was registered.
+     *
+     * @param int $wpUserId The WordPress User Id.
+     */
+    public function user_registered($wpUserId)
+    {
+        if (!isset($_REQUEST['_wpnonce_create-user']) || !isset($_POST['pass1'])) {
+            return;
+        }
+        if (!wp_verify_nonce(sanitize_text_field(wp_unslash($_REQUEST['_wpnonce_create-user'])), 'create-user')) {
+            wp_die('nonce not valid');
+        }
+        $user = new WP_User($wpUserId);
+        $password = sanitize_text_field(wp_unslash($_POST['pass1']));
 
-		return new WP_User( $newUser );
-	}
+        $account = new \stdClass();
+        $account->email = $user->user_email;
+        $account->password = $password;
+        $account->givenName = $user->user_firstname;
+        $account->surname = $user->user_lastname;
+        $account->username = $user->user_login;
 
-	/**
-	 * Force Login of WordPress User
-	 *
-	 * @param WP_User $wpUser The WordPress User.
-	 * @return WP_User
-	 */
-	public function login_wp_user( $wpUser ) {
-		return wp_set_current_user( $wpUser->ID );
-	}
+        $accountObj = $this->spClient->getDataStore()->instantiate(Account::class, $account);
 
-	/**
-	 * Hook callback for when a user was registered.
-	 *
-	 * @param int $wpUserId The WordPress User Id.
-	 * @return void
-	 */
-	public function user_registered( $wpUserId ) {
-		if ( ! isset( $_REQUEST['_wpnonce_create-user'] ) || ! isset( $_POST['pass1'] ) ) {
-			return;
-		}
-		if ( ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_REQUEST['_wpnonce_create-user'] ) ), 'create-user' ) ) {
-			wp_die( 'nonce not valid' );
-		}
-		$user = new WP_User( $wpUserId );
-		$password = sanitize_text_field( wp_unslash( $_POST['pass1'] ) );
+        // setup to add existing meta data to customData
+        $customData = $accountObj->customData;
+        $user_meta_arr = get_user_meta($wpUserId);
+        $user_meta_blacklist = array('user_email', 'first_name', 'last_name', 'nickname');
 
-		$account = new \stdClass();
-		$account->email = $user->user_email;
-		$account->password = $password;
-		$account->givenName = $user->user_firstname;
-		$account->surname = $user->user_lastname;
-		$account->username = $user->user_login;
+        foreach ($user_meta_arr as $key => $user_meta) {
+            if (!in_array($key, $user_meta_blacklist)) {
+                $customData->$key = $user_meta;
+                var_dump($user_meta);
+            }
+        }
 
-		$accountObj = $this->spClient->getDataStore()->instantiate( Account::class, $account );
+        var_dump($accountObj);
 
-		$this->spApplication->createAccount( $accountObj );
+        wp_die();
 
-		wp_set_password( wp_hash_password( wp_generate_password( 32 ) ), $wpUserId );
+        $this->spApplication->createAccount($accountObj);
 
-	}
+        wp_set_password(wp_hash_password(wp_generate_password(32)), $wpUserId);
+    }
 
-	/**
-	 * Register a user from a valid WP_User.
-	 *
-	 * @param WP_User $wpUser   The user being registered.
-	 * @param string  $password The password for the user.
-	 * @return Account|WP_Error|boolean
-	 */
-	public function register_stormpath_user( WP_User $wpUser, $password ) {
-	    try {
-			$account = new \stdClass();
-			$account->email = $wpUser->user_email;
-			$account->password = $password;
-			$account->givenName = $wpUser->user_firstname ?: 'Temp FirstName';
-			$account->surname = $wpUser->user_lastname ?: 'Temp LastName';
-			$account->username = $wpUser->user_login;
+    /**
+     * Register a user from a valid WP_User.
+     *
+     * @param WP_User $wpUser   The user being registered.
+     * @param string  $password The password for the user.
+     *
+     * @return Account|WP_Error|bool
+     */
+    public function register_stormpath_user(WP_User $wpUser, $password)
+    {
+        try {
+            $account = new \stdClass();
+            $account->email = $wpUser->user_email;
+            $account->password = $password;
+            $account->givenName = $wpUser->user_firstname ?: 'Temp FirstName';
+            $account->surname = $wpUser->user_lastname ?: 'Temp LastName';
+            $account->username = $wpUser->user_login;
 
-			$accountObj = $this->spClient->getDataStore()->instantiate( Account::class, $account );
+            $accountObj = $this->spClient->getDataStore()->instantiate(Account::class, $account);
 
-			return $this->spApplication->createAccount( $accountObj );
+            return $this->spApplication->createAccount($accountObj);
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
 
-		} catch (\Exception $e) {
-			return false;
-		}
-	}
+    /**
+     * Hook callback for when a profile was updated.
+     *
+     * @param int    $userId  Id of User.
+     * @param object $oldData old user data.
+     */
+    public function profile_update($userId, $oldData)
+    {
+        $newData = get_userdata($userId);
+        if (!isset($_REQUEST['_wpnonce']) || !isset($_POST['pass1-text'])) {
+            return;
+        }
+        if (!wp_verify_nonce(sanitize_text_field(wp_unslash($_REQUEST['_wpnonce'])), 'update-user_'.$userId)) {
+            wp_die('nonce not valid');
+        }
 
-	/**
-	 * Hook callback for when a profile was updated.
-	 *
-	 * @param int    $userId  Id of User.
-	 * @param object $oldData old user data.
-	 * @return void
-	 */
-	public function profile_update( $userId, $oldData ) {
-		$newData = get_userdata( $userId );
-		if ( ! isset( $_REQUEST['_wpnonce'] ) || ! isset( $_POST['pass1-text'] ) ) {
-			return;
-		}
-		if ( ! wp_verify_nonce( sanitize_text_field( wp_unslash( $_REQUEST['_wpnonce'] ) ), 'update-user_' . $userId ) ) {
-			wp_die( 'nonce not valid' );
-		}
+        // Remove the nag if the password has been changed.
+        if ($newData->user_pass !== $oldData->user_pass) {
+            $user = new WP_User($userId);
+            $this->password_changed($user, sanitize_text_field(wp_unslash($_POST['pass1-text'])));
+        }
+    }
 
-		// Remove the nag if the password has been changed.
-		if ( $newData->user_pass !== $oldData->user_pass ) {
-			$user = new WP_User( $userId );
-			$this->password_changed( $user, sanitize_text_field( wp_unslash( $_POST['pass1-text'] ) ) );
+    /**
+     * Hook Callback for when a password was changed.
+     *
+     * @param WP_User $user     The User.
+     * @param string  $password The Password.
+     */
+    public function password_changed($user, $password)
+    {
+        $accounts = $this->spApplication->accounts->setSearch(['q' => $user->user_email]);
+        if ($accounts->size > 0) {
+            $account = $accounts->getIterator()->current();
 
-		}
-	}
+            $account->password = $password;
+            $account->save();
 
-	/**
-	 * Hook Callback for when a password was changed.
-	 *
-	 * @param WP_User $user     The User.
-	 * @param string  $password The Password.
-	 * @return void
-	 */
-	public function password_changed( $user, $password ) {
-		$accounts = $this->spApplication->accounts->setSearch( [ 'q' => $user->user_email ] );
-		if ( $accounts->size > 0 ) {
-
-			$account = $accounts->getIterator()->current();
-
-			$account->password = $password;
-			$account->save();
-
-			$id = $user->ID;
-			wp_set_password( wp_hash_password( wp_generate_password( 32 ) ), $id );
-		}
-	}
+            $id = $user->ID;
+            wp_set_password(wp_hash_password(wp_generate_password(32)), $id);
+        }
+    }
 }
